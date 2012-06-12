9c9bac7e53d05e5970f05ca08a08428d0915d6e9
jlagram <>
2012-06-12 10:04:20 +0000

    for CMSSW_4_2_8_p7

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Installing IPHC framework from CVS repository"
 echo "----------------------------------------------"
+
 cd $CMSSW_BASE/src
 mkdir IPHCDataFormat
 mkdir MiniTree
@@ -10,8 +11,56 @@ cvs co -d IPHCDataFormat UserCode/SusyIPHC/IPHCDataFormat
 cvs co -d MiniTree UserCode/SusyIPHC/MiniTree
 cvs co -d NTuple UserCode/SusyIPHC/NTuple
 cvs co -d marco UserCode/SusyIPHC/marco
+
+
+#Put the recipe here !!!
+echo "Get PAT recipies"
+
+
+
+#
+echo "PAT RECIPE For 428_patch7!"
+#
+addpkg DataFormats/PatCandidates V06-04-19-02
+addpkg PhysicsTools/PatAlgos V08-06-50
+addpkg PhysicsTools/PatUtils V03-09-18
+addpkg PhysicsTools/PatExamples V00-05-24
+addpkg CommonTools/ParticleFlow B4_2_X_V00-03-00
+addpkg PhysicsTools/SelectorUtils V00-03-24
+addpkg PhysicsTools/UtilAlgos V08-02-14
+
+echo "NEW RECIPE Type1/1p2 MET correction"
+cvs co -r V04-05-07 JetMETCorrections/Type1MET
+cvs co -r V03-03-07 DataFormats/METReco
+
+cvs co -r V02-03-00 JetMETCorrections/Algorithms
+rm -f JetMETCorrections/Algorithms/interface/L1JPTOffsetCorrector.h
+rm -f JetMETCorrections/Algorithms/src/L1JPTOffsetCorrector.cc
+cvs co -r V03-01-00 JetMETCorrections/Objects
+addpkg JetMETCorrections/Modules
+cvs up -r 1.4 JetMETCorrections/Modules/plugins/JetCorrectorOnTheFly.cc
+cvs up -r 1.6 JetMETCorrections/Modules/interface/JetCorrectionProducer.h
+
+
+
+#for eID with likelihood
+cvs co -r V00-03-31 RecoEgamma/ElectronIdentification
+ 
+echo "Now compile using scramv1 b -j 5"
+scramv1 b -j 5
+echo "Compilation finished"
+
+cd IPHCDataFormat/NTFormat/src
+make 
+cd ../../..
+
+cd NTuple/NTupleAnalysis/
+mkdir .lib
+make all
+cd ../../.
+
+
 echo "----------------------------------------------"
 echo "Installation finished"
 echo ""
-
 
